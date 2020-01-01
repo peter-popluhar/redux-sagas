b98d2b3a56dc72ddf9ab27608e6104f0c408058e
Peter Popluhar <peter.popluhar@socialbakers.com>
2020-01-01 15:15:03 +0100

    users saga

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React , {Component} from 'react';
+import {connect} from 'react-redux';
+import {getUsersRequest} from '../actions/users';
+
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.props.getUsersRequest();
+    }
+    render() {
+        return (
+            <div>
+                test
+            </div>
+        );
+    }
+
+}
+
+export default connect(null, {
+    getUsersRequest
+})(App);
